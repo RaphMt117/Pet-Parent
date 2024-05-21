b7d8ce6f8c37077945bbeb4b75cd48776a164ff4
Davi Nicoletto <davinicolettofs7@gmail.com>
2024-05-21 16:37:32 -0300

    Fix: removed template Astro components

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -2,7 +2,20 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#2CBBC3',
+				secondary: '#FF9934',
+				primaryAccent: '#E1F1F2',
+				onPrimary: '#FCFCFC',
+				onSecondary: '#FCFCFC',
+				dark: '#252A34',
+				light: '#EAEAEA'
+			},
+			fontFamily: {
+				'sans' : ['Montserrat', 'ui-sans-serif', 'system-ui']
+			}
+		},
 	},
 	plugins: [],
 }
